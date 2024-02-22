0494f4e2f67679ae468b1066780dce83683a2670
alinisp <alinisperr@gmail.com>
2024-02-22 11:31:01 -0400

    atividade 1

---
+++ atividade01.js
@@ -0,0 +1,25 @@
+var nome = "Alini Perroni"
+var idade = "15"
+console.log("Olá, meu nome é " +nome+" e eu tenho "+idade+" anos!");
+
+console.log('---------------');
+
+console.log('banana'.toUpperCase());
+console.log('BANANA'.toLowerCase());
+
+console.log('---------------');
+
+var nome1 = "Alini Perroni"
+var num1 = 12
+console.log(typeof nome1);
+console.log(typeof num1);
+
+console.log('---------------');
+
+var altura = 1.50
+var peso = 60
+console.log(peso/altura*altura);
+
+console.log('---------------');
+
+var num1 = 
