6df4888d615671c8722f262d79b24e8b5226fe30
alinisp <alinisperr@gmail.com>
2024-02-23 08:43:58 -0400

    atividade 1

--- atividade01.js
+++ atividade01.js
@@ -20,6 +20,19 @@ var altura = 1.50
 var peso = 60
 console.log(peso/altura*altura);
 
+console.log('---------------Operacionais----------------');
+
+var num1 = 9
+var num2 = 78
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1/num2)
+console.log(num1*num2)
+
 console.log('---------------');
 
-var num1 = 
+var idd1 = 16
+var idd2 = 87
+console.log(idd1>idd2);
+
+console.log('---------------');
