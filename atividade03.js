0693f8b0b2957e7474963cec54a1846b85301ebe
alinisp <alinisperr@gmail.com>
2024-03-07 11:45:22 -0400

    atividade 1

---
+++ atividade03.js
@@ -0,0 +1,100 @@
+import ds from 'readline-sync';
+
+let dias= ds.questionInt('Digite o dia da semana:');
+
+switch (dias) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-feira");
+        break;
+    case 3:
+        console.log("Terça-feira");
+        break;
+    case 4:
+        console.log("Quarta-feira");
+        break;
+    case 5:
+        console.log("Quinta-feira");
+        break;
+    case 6:
+        console.log("Sexta-feira");
+        break;
+    case 7:
+        console.log("Sábado");
+        break;
+    default:
+        console.log("Inválido!")
+        break;
+}
+
+console.log('------------------------');
+
+let mes= ds.questionInt('Digite o mes:');
+
+switch (mes) {
+    case 1:
+        console.log("Janeiro");
+        break;
+    case 2:
+        console.log("fevereiro");
+        break;
+    case 3:
+        console.log("Março");
+        break;
+    case 4:
+        console.log("Abril");
+        break;
+    case 5:
+        console.log("Maio");
+        break;
+    case 6:
+        console.log("Junho");
+        break;
+    case 7:
+        console.log("Julho");
+        break;
+
+    case 8:
+        console.log("Agosto");
+        break;
+    case 9:
+        console.log("Setembro");
+        break;
+    case 10:
+        console.log("Outubro");
+        break;
+    case 11:
+        console.log("Novembro");
+        break;
+    case 12:
+        console.log("Dezembro");
+        break;
+    default:
+        console.log("Inválido!")
+        break;
+}
+console.log('------------------------');
+
+let num1 = ds.question("Numero 1:");
+let num2 = ds.question("Numero 2:");
+
+let calcular= ds.question('Qual operacao deseja usar:')
+switch (calcular) {
+    case adição:
+        console.log(num1+num2);
+        break;
+    case subtração:
+        console.log(num1-num2);
+        break;
+    case multiplicação:
+        console.log(num1*num2);
+        break;
+    case Divisão:
+        console.log(num1/num2);
+        break;
+    default:
+        ("Inválido")
+        break;
+}
