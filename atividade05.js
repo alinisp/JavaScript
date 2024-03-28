20a8e9bece378c662e31c5a688cf6ec1bbaf4712
alinisp <alinisperr@gmail.com>
2024-03-28 11:45:41 -0400

    atividade

---
+++ atividade05.js
@@ -0,0 +1,24 @@
+/*01. Crie uma função que receba uma string como
+parâmetro e retorne a mesma string com todas as letras
+em caixa alta.
+function caixaAlta(string){
+    return string.toUpperCase()
+};
+
+console.log(caixaAlta('amavi'));*/
+
+/*02. Crie uma função que receba um número como
+parâmetro e verifique se ele é par ou ímpar. Retorne uma
+string.
+function imPar(numero) {
+    if (numero %2 ===0) return 'Par'
+    else return 'Impar'
+};
+console.log(imPar(5));*/
+
+/*03. Crie uma função que receba dois números como
+parâmetros e retorne a soma deles.*/
+function soma(numero1, numero2) {
+    numero1 + numero2
+};
+console.log(soma(2, 5));
